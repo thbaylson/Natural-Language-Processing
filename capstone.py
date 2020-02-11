c435b255a7c635c176798257de116459cb8fecbe
unknown <thbaylson@gmail.com>
2020-02-11 14:06:57 -0500

    Added basic test suite

---
+++ capstone.py
@@ -0,0 +1,23 @@
+def getTokens(inp):
+    """ Placeholder function to help set up PyTest"""
+    return inp
+
+def getGrammar(tokens):
+    """ Placeholder function to help set up PyTest"""
+    return tokens
+
+def getSyntaxLong(grammar):
+    """ Placeholder function to help set up PyTest"""
+    return grammar
+
+def getSyntaxShort(synLong):
+    """ Placeholder function to help set up PyTest"""
+    return synLong
+
+def getRule(synShort):
+    """ Placeholder function to help set up PyTest"""
+    return synShort
+
+def alwaysTrue():
+    """ Placeholder function to help set up PyTest"""
+    return True
