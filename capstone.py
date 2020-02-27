6cdb4008c2718856258a6dc4ff790b7413153b30
thbaylson <thbaylson@gmail.com>
2020-02-27 09:11:10 -0500

    Added a second testing suite for simple input sentences. Also began implementing the SpaCy library.

--- capstone.py
+++ capstone.py
@@ -1,10 +1,69 @@
+import sys
+import spacy
+import re
+
+def main(inp):
+    nlp = spacy.load("en_core_web_sm")
+
+    processedInput = processInput(inp)
+    print("\nMain [After processInput()]:", processedInput)
+
+    #tokens = getTokens(processedInput)
+    #print("\nMain [After getTokens()]:", tokens)
+
+    grammar = getGrammar(nlp(processedInput))
+    print("\nMain [After getGrammar()]:", grammar)
+
+    syntaxLong = getSyntaxLong(grammar)
+    print("\nMain [After getSyntaxLong()]:", syntaxLong)
+
+    syntaxShort = getSyntaxShort(syntaxLong)
+    print("\nMain [After getSyntaxShort()]:", syntaxShort)
+
+    rule = getRule(syntaxShort)
+    print("\nMain [After getRule()]:", rule)
+
+def processInput(inp):
+    """ 
+    Recieve command line input, search for errors, and split the input on
+    word boundaries.
+    TODO: Write usage notes into README
+    """
+
+    #Match the first command line argument independent of OS
+    matchObject = re.search('capstone.py', '.\\capstone.py')
+    if matchObject != None:
+        #Trims off the command line file call
+        inp = inp[1:]
+    return str(inp)
+
+"""
 def getTokens(inp):
-    """ Placeholder function to help set up PyTest"""
-    return inp
+    Placeholder function to help set up PyTest
+    #SpaCy can tokenize. The call nlp() uses the default model. To speed things up, we may want to define our own model
+    return nlp(inp)
+"""
 
 def getGrammar(tokens):
     """ Placeholder function to help set up PyTest"""
-    return tokens
+    # SpaCy can find tokens and parts of speech at the same time
+    grammar = {}
+
+    # Creates a dictionary with keys being the input words and their values being that input word's P.O.S.
+    for token in tokens:
+        grammar[(token.text).lower()] = [(token.pos_).lower(), (token.dep_).lower()]
+
+    # Remove punction from the grammar
+    keys_to_remove = []
+    for key in grammar:
+        if 'punct' in grammar[key]:
+            keys_to_remove.append(key)
+  
+    for key in keys_to_remove:
+        del grammar[key]
+
+    print(grammar)
+    return grammar
 
 def getSyntaxLong(grammar):
     """ Placeholder function to help set up PyTest"""
@@ -21,3 +80,6 @@ def getRule(synShort):
 def alwaysTrue():
     """ Placeholder function to help set up PyTest"""
     return True
+
+if __name__ == "__main__":
+    main(sys.argv)
