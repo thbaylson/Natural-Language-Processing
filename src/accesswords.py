9a8e99002513a225009c19e3c79869fce1409100
AC Sexton <ac.sexton@specbooks.com>
2020-08-30 23:00:08 -0400

    move to own class file

---
+++ src/accesswords.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class AccessWords(Enum):
+    access =        ["access",      "edit"]
+    append =        ["append",      "edit"]
+    change =        ["change",      "edit"]
+    edit =          ["edit",        "edit"]
+    examine =       ["examine",     "read"]
+    manipulate =    ["manipulate",  "edit"]
+    modify =        ["modify",      "edit"]
+    read =          ["read",        "read"]
+    see =           ["see",         "read"]
+    update =        ["update",      "edit"]
+    use =           ["use",         "edit"]
+    view =          ["view",        "read"]
+    write =         ["write",       "edit"]
